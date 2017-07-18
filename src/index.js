98501e2a9779e7e7f446d1e0e0c9618863bfb895
Isaac Rowntree <isaac.rowntree@redbubble.com>
2017-07-18 12:37:16 +1000

    Step 4: Fire an action from the store and catch it in the reducer
     - The whole state is set up with a default count of 0
     - The state is returned with an addition to the counter on dispatch
     - An extra subscriber is created so you can see the output in console.log

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import { createStore } from 'redux';
 
-const reducer = (state = {}, action) => state;
+const reducer = (state = {count: 0}, action) => {
+  if (action.type === 'INCREMENT') {
+    return Object.assign({}, state, {count: state.count + 1});
+  }
+  return state;
+};
 
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const action = {type: 'INCREMENT'};
+
+store.subscribe(() => (console.log(store.getState())));
+
+store.dispatch(action);
