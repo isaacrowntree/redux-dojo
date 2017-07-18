da6761f96313fbd4c3bdd3a83d69bf29e54b052b
Isaac Rowntree <isaac.rowntree@redbubble.com>
2017-07-18 15:09:36 +1000

    Step 6: Add React! Run
    `yarn add react react-dom babel-core babel-loader babel-preset-env
    babel-preset-es2015 babel-preset-react`

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 var path = require("path");
 
 module.exports = {
-  entry: {
-    app: ["./src/index.js"]
-  },
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
     filename: "bundle.js"
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      }
+    ]
+  },
 };
